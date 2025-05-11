6d545b17e423c33d63bc53d638fe6084897729cd
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-05-12 00:34:10 +0200

    Add task solution

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ASecondOpinion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter seconds: ");
+			string userInput = Console.ReadLine();
+
+			getTime(userInput);
+		}
+
+		private static int getSeconds(string secondsString)
+		{
+			int seconds = Int32.Parse(secondsString);
+			return seconds;
+		}
+
+		private static void getTime(string secondsString)
+		{
+			int secondsSum = getSeconds(secondsString);
+
+			int hours = (secondsSum - secondsSum % 3600) / 3600;
+			int minutes = (secondsSum % 3600 - secondsSum % 3600 % 60) / 60;
+			int seconds = secondsSum % 3600 % 60;
+
+			Console.WriteLine(hours + " : " + minutes + " : " + seconds);
+		}
+	}
+}
